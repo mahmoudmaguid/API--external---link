54c851f3acb638f3782697184cc189d6b16b3a43
Warren Uhrich <hello@warren.codes>
2020-04-29 09:38:13 -0600

    Initial commit.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  $myMessage = 'My First PHP!';
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $myMessage; ?></title>
+</head>
+<body>
+  <h1><?php echo $myMessage; ?></h1>
+</body>
+</html>
