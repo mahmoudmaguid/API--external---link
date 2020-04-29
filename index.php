1ad018803f33beebbadb016c6130670448b41d45
Warren Uhrich <hello@warren.codes>
2020-04-29 10:00:01 -0600

    Add var_dump example with <pre> tag.

--- index.php
+++ index.php
@@ -42,9 +42,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo $myMessage; ?></title>
+  <title><?php echo $myMessage; // Echo is used for outputting strings. ?></title>
 </head>
 <body>
   <h1><?php echo $myMessage; ?></h1>
+  <pre>
+    <?php
+      // Var dump is great for seeing what's inside!
+      var_dump( $myOtherArray );
+    ?>
+  </pre>
 </body>
 </html>
