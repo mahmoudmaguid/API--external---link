3c2d6406b370518fd109ebd106981a58db992e9e
Warren Uhrich <hello@warren.codes>
2020-05-01 11:13:04 -0600

    Add scripts test to api-request-test.

--- api-request-test.php
+++ api-request-test.php
@@ -6,6 +6,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $pageName; ?></title>
+
+  <!-- Script(s) -->
+  <script type="text/javascript" src="./js/scripts.js"></script>
 </head>
 <body>
   <h1><?php echo $pageName; ?></h1>
