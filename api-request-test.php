ff0a56b759245454c7ad6bbf36ce3bfd8de562a5
Warren Uhrich <hello@warren.codes>
2020-05-01 11:05:23 -0600

    Added a webpage for API testing.

---
+++ api-request-test.php
@@ -0,0 +1,16 @@
+<?php
+  $pageName = 'PHP API Request Test';
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $pageName; ?></title>
+</head>
+<body>
+  <h1><?php echo $pageName; ?></h1>
+  <?php include './includes/navigation.php'; ?>
+  <section id="api-test">
+  </section>
+</body>
+</html>
--- includes/navigation.php
+++ includes/navigation.php
@@ -16,6 +16,9 @@
     <li>
       <a href="./oop-and-file.php">OOP and File Retrieval / Reading</a>
     </li>
+    <li>
+      <a href="./api-request-test.php">API Request Test (JS + Endpoint)</a>
+    </li>
   </ul>
 </nav>
 <?php // By default, anything written in a PHP file will be sent to
